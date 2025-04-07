42159b947174dbef39c083976debb387ea124e75
Giovani <giakomogcs@gmail.com>
2025-04-07 19:37:12 -0300

    feat: define permissions for aplication

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,17 @@
-import { defineAbilityFor } from '@saas/auth'
-const ability = defineAbilityFor({ role: 'ADMIN' })
+import { defineAbilityFor, projectSchema } from '@saas/auth'
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('get', 'User')
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user2-id' })
+
+const userCanInviteSomeoneElse = ability.can('create', 'Invite')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCanDeleteProjetc = ability.can('delete', project)
 
 const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+const userCanGetBilling = ability.can('get', 'Billing')
 
 console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteOtherUsers)
+console.log(userCanDeleteProjetc)
 console.log(userCannotDeleteOtherUsers)
+console.log(userCanGetBilling)
