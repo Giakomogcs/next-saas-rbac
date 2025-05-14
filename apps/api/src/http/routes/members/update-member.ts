dad6a92e2e1e9cdced29240242c59074905be2bc
Giovani <giakomogcs@gmail.com>
2025-05-14 20:01:09 -0300

    feat: create a Updarte route for members

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,70 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { createSlug } from '@/utils/create-slug'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Update a member',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          body: z.object({
+            role: z.union([
+              z.literal('ADMIN'),
+              z.literal('MEMBER'),
+              z.literal('BILLING'),
+            ]),
+          }),
+          response: {
+            204: z.object({
+              members: z.null(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to update this member`
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
