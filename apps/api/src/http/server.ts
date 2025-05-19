aeb48999b22944e1f49d1acfb7bd1555cdf47b27
Giovani <giakomogcs@gmail.com>
2025-05-19 19:27:52 -0300

    feat: create a route to reject and another to revoke invites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -36,6 +36,8 @@ import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
 import { acceptInvite } from './routes/invites/accept-invite'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -101,6 +103,8 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
