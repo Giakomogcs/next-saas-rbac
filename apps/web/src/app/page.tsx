3f71bdc5e6270d076297cb912203d32182342d6b
Giovani <giakomogcs@gmail.com>
2025-05-21 20:09:10 -0300

    feat: Configurating shadcn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hello Word</h1>
+  return (
+    <div>
+      <h1>Hello Word</h1>
+      <br />
+      <Button>SignIn</Button>
+    </div>
+  )
 }
